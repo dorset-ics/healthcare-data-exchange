9ee992f8176dace485a75e4c22bc7b358a5590ad
Liam Moat <liam.moat@microsoft.com>
2024-04-15 14:25:04 +0100

    feat: return OperationOutcome for Fhir exceptions

--- src/Api/Exceptions/GlobalExceptionHandler.cs
+++ src/Api/Exceptions/GlobalExceptionHandler.cs
@@ -1,6 +1,10 @@
 using System.Diagnostics;
+using System.Net;
+using Hl7.Fhir.Rest;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using static Hl7.Fhir.Model.OperationOutcome;
+using Task = System.Threading.Tasks.Task;
 
 namespace Api.Exceptions;
 
@@ -8,36 +12,64 @@ public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, IHos
 {
     private const bool IsLastStopInPipeline = true;
 
-    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext,
-        Exception exception, CancellationToken cancellationToken)
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
     {
         var traceId = Activity.Current?.Id ?? httpContext.TraceIdentifier;
-        logger.LogError(exception, "Could not process a request on machine {MachineName} with trace id {TraceId}",
-            Environment.MachineName, traceId);
+        logger.LogError(exception, "Could not process a request on machine {MachineName} with trace id {TraceId}", Environment.MachineName, traceId);
 
-        (int statusCode, string title) = MapException(exception);
-
-        var problemDetails = new ProblemDetails
+        if (exception is FhirOperationException fhirOperationException)
         {
-            Status = statusCode,
-            Title = title,
-            Extensions = { ["traceId"] = traceId },
-            Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}"
-        };
-        if (!environment.IsProduction())
+            await WriteOperationOutcomeAsync(fhirOperationException, httpContext, cancellationToken);
+        }
+        else
         {
-            problemDetails.Detail = exception.Message;
+            await WriteProblemDetails(exception, traceId, httpContext, cancellationToken);
         }
 
-        await httpContext.Response
-            .WriteAsJsonAsync(problemDetails, cancellationToken);
         return IsLastStopInPipeline;
     }
 
-    private static (int statusCode, string title) MapException(Exception exception)
-        => exception switch
+    private async Task WriteOperationOutcomeAsync(FhirOperationException fhirOperationException, HttpContext httpContext, CancellationToken cancellationToken)
+    {
+        var issueType = MapFhirOperationException(fhirOperationException);
+        var operationOutcome = ForException(fhirOperationException, issueType);
+
+        if (!environment.IsDevelopment())
         {
-            TaskCanceledException _ => (StatusCodes.Status504GatewayTimeout, "Request Timeout"),
-            _ => (StatusCodes.Status500InternalServerError, "Internal Server Error")
+            operationOutcome.Issue.ForEach(x => x.Diagnostics = null);
+        }
+
+        httpContext.Response.StatusCode = (int)fhirOperationException.Status;
+        await httpContext.Response.WriteAsJsonAsync(operationOutcome, cancellationToken);
+    }
+
+    private IssueType MapFhirOperationException(FhirOperationException fhirOperationException) => fhirOperationException.Status switch
+        {
+            HttpStatusCode.NotFound => IssueType.NotFound,
+            HttpStatusCode.BadRequest => IssueType.Invalid,
+            _ => IssueType.Exception
         };
+
+    private async Task WriteProblemDetails(Exception exception, string traceId, HttpContext httpContext, CancellationToken cancellationToken)
+    {
+        var (statusCode, title) = MapException(exception);
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = (int)statusCode,
+            Title = title,
+            Extensions = { ["traceId"] = traceId },
+            Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}",
+            Detail = environment.IsDevelopment() ? exception.Message : null
+        };
+
+        httpContext.Response.StatusCode = (int)statusCode;
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+    }
+
+    private (HttpStatusCode httpStatusCode, string title) MapException(Exception exception) => exception switch
+    {
+        TaskCanceledException _ => (HttpStatusCode.GatewayTimeout, "Request Timeout"),
+        _ => (HttpStatusCode.InternalServerError, "Internal Server Error")
+    };    
 }
--- tests/Unit.Tests/Api/Exceptions/GlobalExceptionHandlerTests.cs
+++ tests/Unit.Tests/Api/Exceptions/GlobalExceptionHandlerTests.cs
@@ -1,23 +1,85 @@
 using System.Diagnostics;
+using System.Net;
 using System.Text.Json;
 using Api.Exceptions;
+using Core.Pds.Exceptions;
+using Hl7.Fhir.Model;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using static Hl7.Fhir.Model.OperationOutcome;
+using Task = System.Threading.Tasks.Task;
 
 namespace Unit.Tests.Api.Exceptions;
 
 public class GlobalExceptionHandlerTests
 {
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower };
+
     [Theory]
-    [InlineData("Development", typeof(TaskCanceledException), StatusCodes.Status504GatewayTimeout, "Request Timeout")]
-    [InlineData("Production", typeof(TaskCanceledException), StatusCodes.Status504GatewayTimeout, "Request Timeout")]
-    [InlineData("Development", typeof(Exception), StatusCodes.Status500InternalServerError, "Internal Server Error")]
-    [InlineData("Production", typeof(Exception), StatusCodes.Status500InternalServerError, "Internal Server Error")]
-    public async Task TryHandleAsync_ShouldWriteProblemDetailsToResponse_WhenExceptionOccurs(string environmentName, Type exceptionType, int expectedStatusCode,
-        string expectedTitle)
+    [InlineData("Development", typeof(PdsSearchFailedException), HttpStatusCode.BadRequest, IssueType.Invalid)]
+    [InlineData("Production", typeof(PdsSearchPatientNotFoundException), HttpStatusCode.NotFound, IssueType.NotFound)]
+    public async Task TryHandleAsync_ShouldWriteOperationOutcomeToResponse_WhenFhirOperationExceptionOccurs(string environmentName, Type exceptionType, HttpStatusCode expectedStatusCode, IssueType expectedIssueType)
+    {
+        var exception = (Exception)Activator.CreateInstance(exceptionType, "Test Exception")!;
+        var cancellationToken = new CancellationToken();
+        var (handler, httpContext) = CreateHandler(environmentName);
+
+        var isLastStopInPipeline = await handler.TryHandleAsync(httpContext, exception, cancellationToken);
+        isLastStopInPipeline.ShouldBeTrue();
+        httpContext.Response.StatusCode.ShouldBe((int)expectedStatusCode);
+
+        var expectedOperationOutcome = new OperationOutcome
+        {
+            Issue =
+                [
+                    new() 
+                    { 
+                        Severity = IssueSeverity.Error,
+                        Code = expectedIssueType,
+                        Diagnostics = environmentName == Environments.Development ? exception.Message : null
+                    }
+                ]
+        };
+
+        var expectedBody = JsonSerializer.Serialize(expectedOperationOutcome, _jsonSerializerOptions);
+
+        var body = await ReadResponseBody(httpContext, cancellationToken);
+        body.ShouldBe(expectedBody);
+    }
+
+    [Theory]
+    [InlineData("Development", typeof(TaskCanceledException), HttpStatusCode.GatewayTimeout, "Request Timeout")]
+    [InlineData("Production", typeof(TaskCanceledException), HttpStatusCode.GatewayTimeout, "Request Timeout")]
+    [InlineData("Development", typeof(Exception), HttpStatusCode.InternalServerError, "Internal Server Error")]
+    [InlineData("Production", typeof(Exception), HttpStatusCode.InternalServerError, "Internal Server Error")]
+    public async Task TryHandleAsync_ShouldWriteProblemDetailsToResponse_WhenExceptionOccurs(string environmentName, Type exceptionType, HttpStatusCode expectedStatusCode, string expectedTitle)
+    {
+        var exception = (Exception)Activator.CreateInstance(exceptionType, "Test Exception")!;
+        var cancellationToken = new CancellationToken();
+        var (handler, httpContext) = CreateHandler(environmentName);
+
+        var isLastStopInPipeline = await handler.TryHandleAsync(httpContext, exception, cancellationToken);
+        isLastStopInPipeline.ShouldBeTrue();
+        httpContext.Response.StatusCode.ShouldBe((int)expectedStatusCode);
+
+        var expectedProblemDetails = new ProblemDetails
+        {
+            Status = (int)expectedStatusCode,
+            Title = expectedTitle,
+            Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}",
+            Extensions = { ["traceId"] = Activity.Current?.Id ?? httpContext.TraceIdentifier },
+            Detail = environmentName == Environments.Development ? exception.Message : null
+        };
+        var expectedBody = JsonSerializer.Serialize(expectedProblemDetails, _jsonSerializerOptions);
+
+        var body = await ReadResponseBody(httpContext, cancellationToken);
+        body.ShouldBe(expectedBody);
+    }
+
+    private (GlobalExceptionHandler, HttpContext) CreateHandler(string environmentName)
     {
         var logger = Substitute.For<ILogger<GlobalExceptionHandler>>();
         var environment = Substitute.For<IWebHostEnvironment>();
@@ -29,34 +91,14 @@ public class GlobalExceptionHandlerTests
         httpContext.Request.Method.Returns("GET");
         httpContext.Request.Path.Returns(new PathString("/test"));
 
-        var exception = (Exception)Activator.CreateInstance(exceptionType, "Test Exception")!;
-        var cancellationToken = new CancellationToken();
-
         var handler = new GlobalExceptionHandler(logger, environment);
+        return (handler, httpContext);
+    }
 
-        await handler.TryHandleAsync(httpContext, exception, cancellationToken);
-
+    private async Task<string> ReadResponseBody(HttpContext httpContext, CancellationToken cancellationToken)
+    {
         httpContext.Response.Body.Seek(0, SeekOrigin.Begin);
         var reader = new StreamReader(httpContext.Response.Body);
-        var body = await reader.ReadToEndAsync(cancellationToken);
-
-        var expectedProblemDetails = new ProblemDetails
-        {
-            Status = expectedStatusCode,
-            Title = expectedTitle,
-            Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}",
-            Extensions = { ["traceId"] = Activity.Current?.Id ?? httpContext.TraceIdentifier }
-        };
-        if (environmentName == Environments.Development)
-        {
-            expectedProblemDetails.Detail = exception.Message;
-        }
-
-#pragma warning disable CA1869
-        var jsonSerializerOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower };
-#pragma warning restore CA1869
-        var expectedJson = JsonSerializer.Serialize(expectedProblemDetails, jsonSerializerOptions);
-
-        body.ShouldBe(expectedJson);
+        return await reader.ReadToEndAsync(cancellationToken);
     }
 }
