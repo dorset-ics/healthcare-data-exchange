590e67bfbc8113f5289e135f53182351fcf285f0
ArpitGaur <arpitgaur@microsoft.com>
2024-04-18 12:46:49 +0100

    remove delete binding from patient module.

--- src/Api/Modules/PatientModule.cs
+++ src/Api/Modules/PatientModule.cs
@@ -16,25 +16,7 @@ public class PatientModule : CarterModule
         var patientsGroup = app.MapGroup("Patient");
         patientsGroup.MapGet("/", Search).WithName("GetPatients");
         patientsGroup.MapGet("/{id}", GetPatientById).WithName("GetPatientById");
-        patientsGroup.MapDelete("/{id}", DeletePatientById).WithName("DeletePatientById");
     }
-
-    private static async Task<IResult> DeletePatientById(string id, IValidator<NhsNumber> validator, IPdsService pdsService, ILogger<PatientModule> logger)
-    {
-        id = id.Replace(" ", string.Empty);
-
-        var validationResult = await validator.ValidateAsync(new NhsNumber(id));
-        if (!validationResult.IsValid)
-            return TypedResults.BadRequest(validationResult.Errors.Select(error => error.ErrorMessage).ToList());
-
-        var deletePatientResult = await pdsService.DeletePatientById(id);
-
-        return deletePatientResult.Match(
-            onSuccess: TypedResults.Ok,
-            onFailure: PatientNotFoundExceptionToResult
-        );
-    }
-
     private static async Task<IResult> GetPatientById(string id, IValidator<NhsNumber> validator, IPdsService pdsService, ILogger<PatientModule> logger)
     {
         id = id.Replace(" ", string.Empty);
